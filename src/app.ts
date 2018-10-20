0f7ee22e07944d9fba26c5833fe602a36b259a5d
Niko Pettersen <niko.pettersen@gmail.com>
2018-10-20 23:00:58 +0300

    Fix errors reported by TSLint

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,27 @@
 import { BrowserWindow } from 'electron';
 
 export default class App {
-	static BrowserWindow;
-	static Electron: Electron.App;
-	static mainWindow: Electron.BrowserWindow;
+	public static BrowserWindow;
+	public static Electron: Electron.App;
+	public static mainWindow: Electron.BrowserWindow;
 
 	// Initialize the application
-	static init( app: Electron.App, browserWindow: typeof BrowserWindow ) {
+	public static init( app: Electron.App, browserWindow: typeof BrowserWindow ) {
 		App.BrowserWindow = browserWindow;
 		App.Electron = app;
 		App.Electron.on( 'activate', App.onActivate );
 		App.Electron.on( 'ready', App.onReady );
 		App.Electron.on( 'window-all-closed', App.onWindowAllClosed );
-	};
+	}
 
 	private static createWindow() {
 		App.mainWindow = new App.BrowserWindow( {
 			height: 600,
-			width: 800
+			width: 800,
 		} );
 		App.mainWindow.loadURL( 'file://' + __dirname + '/../../index.html' );
 		App.mainWindow.on( 'closed', App.onClose );
-	};
+	}
 
 	private static onActivate() {
 		// On OS X it's common to re-create a window in the app when the
@@ -29,21 +29,21 @@ export default class App {
 		if ( null === App.mainWindow ) {
 			App.createWindow();
 		}
-	};
+	}
 
 	// Dereference the window object, usually you would store windows
 	// in an array if your app supports multi windows, this is the time
 	// when you should delete the corresponding element.
 	private static onClose() {
 		App.mainWindow = null;
-	};
+	}
 
 	// This method will be called when Electron has finished
 	// initialization and is ready to create browser windows.
 	// Some APIs can only be used after this event occurs.
 	private static onReady() {
 		App.createWindow();
-	};
+	}
 
 	private static onWindowAllClosed() {
 		// On OS X it is common for applications and their menu bar
@@ -51,5 +51,5 @@ export default class App {
 		if ( 'darwin' !== process.platform ) {
 			App.Electron.quit();
 		}
-	};
-};
+	}
+}
